32e81d7399ece515cc48850ef5dca527708ba030
Artur Bier <izatop@gmail.com>
2015-09-23 16:44:40 +0300

    Fix wrong package export.

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-var Pickpoint = require('./build/index.js');
-var api = new Pickpoint('apitest', 'apitest', {test: true});
-var error = (e) => { console.log(e.stack); };
-
-/*api.getCities().then(function (result) {
-    "use strict";
-    console.log(result);
-}).catch(error);*/
-
-api.logout().then(function (result) {
-    "use strict";
-    console.log(result);
-}).catch(error);
+exports = module.exports = require('./build/index.js');
