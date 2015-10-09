009454bc1097b34646c9b4580c1bf36fb58d6851
Artur Bier <izatop@gmail.com>
2015-10-09 11:45:20 +0300

    wrong a module name case

--- src/datatype.ts
+++ src/datatype.ts
@@ -1,6 +1,6 @@
 import {Request} from './datatype/request';
 import {Messages} from './datatype/messages';
-import {Response} from './datatype/Response';
+import {Response} from './datatype/response';
 
 export {
     Request,
